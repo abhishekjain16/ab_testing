941205d40099f355fc53a5e8a523d60ddc949fef
abhishek <abhishek.jain@vinsol.com>
2014-01-29 22:23:22 +0530

    add render percentage, add method to fetch variation according to probability provided by admin, add js to use the method for the same

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -3,8 +3,24 @@ class Variation < ActiveRecord::Base
 
   before_save :strip_new_line, :if => :data_changed?
 
-
   def strip_new_line
     self.data.squish!
+  end
+
+  def self.fetch_with_probablity_and_percentage
+    hash = {}
+    all.each do |v|
+      hash[v] = v.render_percentage
+    end
+    picker(hash)
+  end
+
+  def self.picker(options)
+    current, max = 0, options.values.inject(:+)
+    random_value = rand(max) + 1
+    options.each do |key,val|
+       current += val
+       return key if random_value <= current
+    end
   end
 end
